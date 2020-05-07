e1f7d4311af646e5d44e0bb89292a13f15b03f1f
Jayashri <jayashri1394@gmail.com>
2020-05-07 02:20:32 +0200

    changed as handlebar change3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const hbs = require('handlebars')
+const hbs = require('hbs')
 const geocode = require('../utils/geocode')
 const forecast = require('../utils/forecast')
 
